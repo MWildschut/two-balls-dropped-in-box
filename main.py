f97dd4b0cd25a413d3241606d9ba5d51a901e4c8
MWildschut <wildschutmariza@gmail.com>
2024-10-06 17:40:45 +0200

    updated code to new project idea

--- main.py
+++ main.py
@@ -1,83 +1,50 @@
 import numpy as np
-import pandas as pd
-import plotly.express as px
-import matplotlib.pyplot as plt
-import matplotlib.animation as an
+
+def DropTwoBalls(x1,y1,vx1,vy1,x2,y2,vx2,vy2):
+    r1 = 0.5
+    r2 = 0.5
+    g = 9.81
+    Dt = 0.1
+    t = np.arrange(0,10,Dt)
+    Wall1 = 0
+    Wall2 = 10
+    Floor = 0
 
 
-r = 0.1
-m = 1
-y0 = 3
-vx0 = 0
-vy0 = 0
+    for i in range(t):
+        vy1 = vy1 - g*Dt
+        vy2 = vy2 - g*Dt
+        x1 = x1 + vx1
+        x2 = x2 + vx2
+        y1 = y1 + vy1
+        y2 = y2 + vy2
+        distance = np.sqrt((x1-x2)**2 + (y1 - y2)**2)
+    
+        if x1 - r1 <= Wall1 or x1 + r1 <= Wall2:
+        vx1 = -vx1
+    
+        if x2 - r2 <= Wall1 or x2 + r2 <= Wall2:
+        vx2 = -vx2
 
-#def DropInWater(r,m,y0,vx0,vy0):
-y = y0
-x = 1
-vx = vx0
-vy = vy0
-Dt = 0.01
-t = 0
-Tmax = 1
-g = 9.81
-Fg = g * m #gravitational force 
-V = 4/3*np.pi*r**3
-A = np.pi*r**2
-Fb = 0
-Cd = 0.47   #drag coefficent of a sphere
-Pw = 998    #density of water
-Pa = 1.225  #average density of air
-ypos = []
-xpos = []
-T = []
-v = np.sqrt(vy0**2 + vx0**2) 
+        if y1 - r1 <= Floor:
+        vy1 = -vy1
+    
+        if y2 - r2 <= Floor:
+        vy2 = -vy2
+  
+        if distance <= r1+r2:
+            vx1, vy1, vx2, vy2 = BallCollision(x1,x2,y1,y2,vx1,vy1,vx2,vy2)
+    
 
-while t < Tmax:
-    if y-r >= 0:     #if the ball is in the air
-        Fd = 1/2 * Pa * vy**2 * Cd * A  #drag of the ball in the air
-        Fb = Pa * V *g #bouyancy of the ball in air (because why not)
-        
-    elif y+r <= 0:   #if the ball is fully under water
-        Fd = 1/2 * Pw * vy**2 * Cd * A    #drag of the ball in the water
-        Fb = Pw * V *g     #bouyancy of the ball in water
-        
-    else:   #if the ball is partially under water
-        h = r - y   #height of the ball that is under water
-        Vw = ((np.pi*h**2)/3)*(3*r - h)     #volume of the ball that is under water, so the volume of displaced water
-        Fb = Pw * Vw *g     #bouyancy of the ball in water
-
-        if vy < 0: #ball is going downward
-            if y <= 0:
-                Fd = 1/2 * Pw * vy**2 * Cd * A
-            else:
-                a = np.sqrt(r**2-y**2)
-                Aw = np.pi * a**2
-                Aa = A - Aw 
-                Fdw = 1/2 * Pw * vy**2 * Cd * Aw
-                Fda = 1/2 * Pa * vy**2 * Cd * Aa
-                Fd = Fda + Fdw
-        else: #ball is going upwards or not moving (which results in a drag of 0)
-            if y >= 0:
-                Fd = 1/2 * Pa * vy**2 * Cd * A
-            else:
-                a = np.sqrt(r**2-y**2)
-                Aa = np.pi * a**2
-                Aw = A - Aa 
-                Fdw = 1/2 * Pw * vy**2 * Cd * Aw
-                Fda = 1/2 * Pa * vy**2 * Cd * Aa
-                Fd = Fda + Fdw
-
-    if vy > 0:
-        Fb = -Fd    #if the velocity is positive and the ball is moving upwards the drag is downwards, so negative
-    Fy = Fb + Fd - Fg
-    ay = Fy/m
-    vy = vy + ay*Dt                                         #calculation of velocity in the y direction
-    y = y + vy                                              #calculation of position in the y direction
-    ypos = np.append(ypos, y)                                      #the value of y is added to the array
-    T = np.append(T,t)
-    v = np.sqrt(vy**2 + vx**2)
-    t = t + Dt
-print(ypos)
-
-
+def BallCollision(x1,x2,y1,y2,vx1,vy1,vx2,vy2):
+    Phi = np.arctan((y2-y1)/(x2-x1))
+    v1 = np.sqrt(vx1**2 + vy1**2)
+    v2 = np.sqrt(vx2**2 + vy2**2)
+    Theta1 = np.arctan(vy1/vx1)
+    Theta2 = np.arctan(vy2/vx2)
+    vx1 = v2 * np.cos(Theta2 - Phi)*np.cos(Phi) + v1 * np.sin(Theta1 - Phi) * np.cos(Phi + np.pi/2)
+    vy1 = v2 * np.cos(Theta2 - Phi)*np.sin(Phi) + v1 * np.sin(Theta1 - Phi) * np.sin(Phi + np.pi/2)
+    vx2 = v1 * np.cos(Theta2 - Phi)*np.cos(Phi) + v2 * np.sin(Theta1 - Phi) * np.cos(Phi + np.pi/2)
+    vy1 = v1 * np.cos(Theta2 - Phi)*np.sin(Phi) + v2 * np.sin(Theta1 - Phi) * np.sin(Phi + np.pi/2)
+    return vx1, vy1, vx2, vy2 
 
