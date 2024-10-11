5a4474d2240c537ac725e8a0c84c8c9ee28d80bb
MWildschut <wildschutmariza@gmail.com>
2024-10-11 18:15:37 +0200

    update code in temp branch, fixed markersize

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def DropTwoBalls(x1,y1,vx1,vy1,x2,y2,vx2,vy2,r1 = 0.5, r2 = 0.5, LeftWall = 0, R
     g = -9.81        #gravitational acceleration
     Dt = 0.01       #time step
     t = 0           #starttime of the simulation
-    tmax = 30       #total run time of the simulation
+    tmax = 10       #total run time of the simulation
 
     #Empty arrays for the x and y coordinates of both balls 
     x1pos =[]
@@ -27,8 +27,7 @@ def DropTwoBalls(x1,y1,vx1,vy1,x2,y2,vx2,vy2,r1 = 0.5, r2 = 0.5, LeftWall = 0, R
         x2 += vx2*Dt
         y1 += vy1*Dt
         y2 += vy2*Dt
-        distance = np.sqrt((x1-x2)**2 + (y1 - y2)**2) #distance between the 2 balls
-    
+            
         x1,vx1 = CheckCollisions(x1,vx1,r1,LeftWall,RightWall)  #checks if ball 1 hits a wall
         x2,vx2 = CheckCollisions(x2,vx2,r2,LeftWall,RightWall)  #checks if ball 2 hits a wall
         y1,vy1 = CheckCollisions(y1,vy1,r1,Floor,Ceiling)       #checks if ball 1 hits a floor or ceiling
@@ -60,8 +59,8 @@ def CheckCollisions(x, v, r, wall1, wall2):
 
     
 def BallCollision(v1,v2,m1,m2):
-    v2n = (2*m1*v1 + m2*v2-m1*v2)/(m1+m2)
-    v1n = (m1*v1+m2*v2-m2*v2n)/m1
+    v2n = (2*m1*v1 + m2*v2-m1*v2)/(m1+m2) #wet van behoud van momenten
+    v1n = (m1*v1 + m2*v2 - m2*v2n)/m1
     return v1n, v2n #returns the new velocities
 
 def Separate(x1,y1,x2,y2,r1,r2,LeftWall,RightWall,Floor,Ceiling, distance):
@@ -71,22 +70,26 @@ def Separate(x1,y1,x2,y2,r1,r2,LeftWall,RightWall,Floor,Ceiling, distance):
         Phi = -np.pi/2    #if x1 equals x2 the calculation for phi doesn't work since it devides by 0. In this case phi should be 90 degrees
     else:
         Phi = np.arctan((y2-y1)/(x2-x1))    #angle between middle point of the 2 balls when they collide
+
+    if x2 < x1:
+        Phi += np.pi #flip
+    
     x1 += np.cos(Phi)*0.5*distance
     y1 += np.sin(Phi)*0.5*distance
     x2 -= np.cos(Phi)*0.5*distance
     y2 -= np.sin(Phi)*0.5*distance 
 
     if x1 + r1 > RightWall or x1 - r1 < LeftWall:
-        x2 -= np.cos(Phi)*distance #x2 is moved
+        x2 -= np.cos(Phi)*0.5*distance #x2 is moved
         x1 -= np.cos(Phi)*0.5*distance #x1 is moved back to where it didn't collide with the wall
     if x2 + r2 > RightWall or x2 - r2 < LeftWall:
-        x1 += np.cos(Phi)*distance
+        x1 += np.cos(Phi)*0.5*distance
         x2 += np.cos(Phi)*0.5*distance
     if y1 + r1 > Ceiling or y1 - r1 < Floor:
-        y2 -= np.cos(Phi)*distance
+        y2 -= np.cos(Phi)*0.5*distance
         y1 -= np.sin(Phi)*0.5*distance
     if y2 + r2 > Ceiling or y2 - r2 < Floor:
-        y1 += np.cos(Phi)*distance
+        y1 += np.cos(Phi)*0.5*distance
         y2 += np.sin(Phi)*0.5*distance
     return x1, y1, x2, y2
 
@@ -96,22 +99,24 @@ def Animate(x1pos,y1pos,x2pos,y2pos,Dt,r1,r2,LeftWall, RightWall, Floor, Ceiling
     ax.set_aspect("equal")      #sets the axes to the same scale
     plt.xlabel("x(m)")
     plt.ylabel("y(m)")
-    point1, = ax.plot(0,1, marker="o", markersize = 50*r1, label = "ball 1")    #marker for ball 1 is made
-    point2, = ax.plot(0,1, marker="o", markersize = 50*r2, label = "ball 2")    #marker for ball 2 is made
-    plt.legend(loc = "upper right", markerscale = 0.5)  #the legend is made
+    M = ax.transData.get_matrix()
+    xscale = M[0,0]
+    point1, = ax.plot(0,1, marker="o", markersize = r1*xscale, label = "ball 1")    #marker for ball 1 is made
+    point2, = ax.plot(0,1, marker="o", markersize = r2*xscale, label = "ball 2")    #marker for ball 2 is made
+    plt.legend(loc = "upper right", markerscale = 0.01*xscale)  #the legend is made
 
-    interval = 100
+    interval = 50
     delay = interval/(Dt*1000)
     def update(frame): #function to update the dot every timeframe, this funcion is called in the animate function
         n = int(frame * delay)
         point1.set_data([x1pos[n]],[y1pos[n]])  #set the location of ball 1 to a certain timeframe
         point2.set_data([x2pos[n]],[y2pos[n]])  #set the location of ball 2 to a certain timeframe
-        return point1, point2,  #returns the new locations of the balls
+        return point1, point2
          
     ani = animation.FuncAnimation(fig, update, interval = interval, frames=int(len(x1pos)/delay), repeat = False) #animates the ball using the update function
     plt.show()
 
 
-DropTwoBalls(x1=2,y1=9,vx1=1,vy1=0,x2=8,y2=8,vx2=-2,vy2 =0) 
+DropTwoBalls(x1=2,y1=8,vx1=5,vy1=0,x2=5,y2=7,vx2=8,vy2 =0,r1 = 1,m1 = 2) 
 
 
