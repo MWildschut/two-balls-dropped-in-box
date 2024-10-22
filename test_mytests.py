271dd96a94bd69ab776109bc2ea1150d5c5b68e7
MWildschut <wildschutmariza@gmail.com>
2024-10-22 18:33:30 +0200

    Small changes to test file due to changes made in main.

--- test_mytests.py
+++ test_mytests.py
@@ -5,16 +5,16 @@ def test_inputs():
     x2 = [4, 1] #valid value, or overlaps with x1
     r1 = [0.5, -0.5, 0, 20] #valid value, negative, zero, or too large
     m1 = [1, -1,0] #valid value, negative or zero
-    RightWall = [10, -10] #valid value, or lower than left wall
+    BoxWidth = [10, -10] #valid value, or lower than left wall
     tmax = [20, -3, 0] #valid value, negative or zero
     z = 0 #variable to check if the first iteration has been done
     for i in range(len(x1)):
         for j in range(len(x2)):
             for k in range(len(r1)):
                 for l in range(len(m1)):
-                    for n in range(len(RightWall)):
+                    for n in range(len(BoxWidth)):
                         for o in range(len(tmax)):
-                            result = m.CheckInputs(x1[i],3,0,0,x2[j],3,0,0,r1[k],0.5,m1[l],1,0,RightWall[n],0,10,tmax[o])
+                            result = m.CheckInputs(x1[i],3,0,0,x2[j],3,0,0,r1[k],0.5,m1[l],1,BoxWidth[n],10,tmax[o])
                             if z==0:
                                 z += 1
                                 assert result == None #if the first value for everything gets taken there should not be an error
@@ -27,9 +27,9 @@ def test_movement():
     assert x == 1.1 and vy == -1 and y == 7.9
 
 def test_wallcollision():
-    x, v = m.CollisionWall(x=1, v=1, r= 0.5, wall1=0, wall2=10) #check if nothing happens if there is no collision
+    x, v = m.CollisionWall(x=1, v=1, r= 0.5, wall=10) #check if nothing happens if there is no collision
     assert x == 1 and v == 1
-    x, v = m.CollisionWall(x=0,v=-1,r=1, wall1=0, wall2=10) #check if collision happens correctly if there is collision
+    x, v = m.CollisionWall(x=0,v=-1,r=1, wall=10) #check if collision happens correctly if there is collision
     assert x == 1 and v == 1
 
 def test_ballcollision():
@@ -37,13 +37,13 @@ def test_ballcollision():
     assert v1n == 2 and v2n == 1
 
 def test_separation():
-    x1, y1, x2, y2 = m.Separate(x1=5,y1=7,x2=6,y2=7,r1=1,r2=1,LeftWall=0,RightWall=10,Floor=0,Ceiling=10, distance=-1)
+    x1, y1, x2, y2 = m.Separate(x1=5,y1=7,x2=6,y2=7,r1=1,r2=1,BoxWidth=10,BoxHeight=10, distance=-1)
     assert x1 == 4.5 and y1 == 7 and x2 == 6.5 and y2 == 7
-    x1, y1, x2, y2 = m.Separate(x1=8,y1=7,x2=9,y2=7,r1=1,r2=1,LeftWall=0,RightWall=10,Floor=0,Ceiling=10, distance=-1)
+    x1, y1, x2, y2 = m.Separate(x1=8,y1=7,x2=9,y2=7,r1=1,r2=1,BoxWidth=10,BoxHeight=10, distance=-1)
     assert x1 == 7 and y1 == 7 and x2 == 9 and y2 == 7
 
 def test_animate():
-    result = m.Animate([],[],[],[],0.1,0.5,0.5,0, 10, 0,10)
+    result = m.Animate([],[],[],[],0.1,0.5,0.5,10,10)
     assert result == "Error"
-    result = m.Animate([3,4],[1,2],[1,2],[4,3,2],0.1,0.5,0.5,0, 10, 0,10)
+    result = m.Animate([3,4],[1,2],[1,2],[4,3,2],0.1,0.5,0.5,10,10)
     assert result == "Error"
